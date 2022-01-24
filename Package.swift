35d410adbcde8552e5d9571f8ed9bdcc41aa9c27
runner <runner@Mac-1643037341856.local>
2022-01-24 15:24:15 +0000

    release/2.1.0

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
     dependencies: [
         .package(name: "ZendeskSDKConversationKit",
                  url: "https://github.com/zendesk/sdk_conversation_kit_ios",
-                 from: "0.7.0"),
+                 from: "0.8.0"),
         .package(name: "ZendeskSDKUIComponents",
                  url: "https://github.com/zendesk/sdk_ui_components_ios",
-                 from: "0.7.0"),
+                 from: "0.7.1"),
         .package(name: "ZendeskSDK",
-                 url: "https://github.com/zendesk/sdk_zendesk_ios",
-                 from: "1.0.0")
+                 url: "https://github.com/zendesk/sdk_ios",
+                 from: "1.1.0")
     ],
     targets: [
         .binaryTarget(
