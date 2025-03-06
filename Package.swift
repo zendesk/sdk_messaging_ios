e42651884eac398c58f49fe815da931e1df0a84f
runner <runner@zendesk.com>
2025-03-06 13:30:49 +0000

    [RELEASE] New version 2.29.0

--- Package.swift
+++ Package.swift
@@ -17,27 +17,27 @@ let package = Package(
         .package(
             name: "ZendeskSDKConversationKit",
             url: "https://github.com/zendesk/sdk_conversation_kit_ios",
-            from: "10.0.0"
+            from: "11.0.0"
         ),
         .package(
             name: "ZendeskSDKUIComponents",
             url: "https://github.com/zendesk/sdk_ui_components_ios",
-            from: "12.0.0"
+            from: "13.0.0"
         ),
         .package(
             name: "ZendeskSDK",
             url: "https://github.com/zendesk/sdk_zendesk_ios",
-            from: "3.6.0"
+            from: "3.7.0"
         ),
         .package(
             name: "ZendeskSDKCoreUtilities",
             url: "https://github.com/zendesk/sdk_core_utilities_ios",
-            from: "5.0.0"
+            from: "6.0.0"
         ),
         .package(
             name: "ZendeskSDKGuideKit",
             url: "https://github.com/zendesk/sdk_guide_kit_ios",
-            from: "2.1.0"
+            from: "2.2.0"
         )
     ],
     targets: [
